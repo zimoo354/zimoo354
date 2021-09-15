503726197238cad3a6659bde5b37e257a512464a
Charlie Ruiz <charlie.ruiz@resideo.com>
2021-09-15 11:29:35 -0500

    feat: adding react site

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import CharlieIcon from "./media/charlie.png";
+
+const links = [
+  {
+    text: "github",
+    url: "https://github.com/zimoo354",
+  },
+  {
+    text: "linkedin",
+    url: "https://www.linkedin.com/in/zimoo354/",
+  },
+  {
+    text: "twitter",
+    url: "https://twitter.com/zimoo354",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={CharlieIcon} alt="Charlie Ruiz" />
+        <div className="name">
+          <h1>Charlie Ruiz</h1>
+          <h2>Software engineer</h2>
+        </div>
+      </header>
+      <article>
+        <p>
+          I’m a mexican software engineer. I love to create amazing things and I
+          do code @ <a href="https://resideo.com">Resideo</a>. Based in Austin,
+          TX 
+        </p>
+      </article>
+      <footer>
+        <ul>
+          {links.map((i) => (
+            <li>
+              <a href={i.url}>{i.text}</a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
