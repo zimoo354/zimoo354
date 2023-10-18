aa7615577e98a5a32857392826197e119ab543fe
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-10-18 16:27:02 -0500

    oops...env files not ignored lol

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
+import { PRODUCTION } from "./lib/env";
 
 const config: Config = {
-  // darkMode: "class",
+  darkMode: PRODUCTION ? "media" : "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
