31a9c68fd9d9076cbf98eecb05b45f6bbbdbde37
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-10-19 17:42:33 -0500

    feat: adding videos to portfolio

--- constants/content.tsx
+++ constants/content.tsx
@@ -144,32 +144,34 @@ export const CONTENT: Record<string, any> & {
     {
       title: "Kodapendant",
       videoUrl: "/videos/kodapendant.mp4",
+      imageUrl: "/videos/kodapendant.webp",
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis at, consequatur cum quo deleniti nam sed soluta, iste facere eius odit optio veniam quidem rerum qui fugit adipisci magni quam.",
-      imageUrl: "https://via.placehold.it/640x480?text=Kodapendant",
-      link: "#",
+      link: "https://kodapendant.otherside.xyz/",
     },
     {
       title: "Battle.Town",
       videoUrl: "/videos/bt.mp4",
+      imageUrl: "/videos/bt.webp",
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis at, consequatur cum quo deleniti nam sed soluta, iste facere eius odit optio veniam quidem rerum qui fugit adipisci magni quam.",
-      imageUrl: "https://via.placehold.it/640x480?text=Bt",
-      link: "#",
+      link: "https://battle.town/",
     },
     {
       title: "Slipstream",
       videoUrl: "/videos/slipstream.mp4",
+      imageUrl: "/videos/slipstream.webp",
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis at, consequatur cum quo deleniti nam sed soluta, iste facere eius odit optio veniam quidem rerum qui fugit adipisci magni quam.",
-      imageUrl: "https://via.placehold.it/640x480?text=Slipstream",
+      link: "https://10ktf.shop/",
     },
     {
       title: "Renga",
       videoUrl: "/videos/renga.mp4",
+      imageUrl: "/videos/renga.webp",
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis at, consequatur cum quo deleniti nam sed soluta, iste facere eius odit optio veniam quidem rerum qui fugit adipisci magni quam.",
-      imageUrl: "https://via.placehold.it/640x480?text=Renga",
+      link: "https://renga.art/",
     },
   ],
   socialLinks: [
