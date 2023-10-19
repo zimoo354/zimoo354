dcff5de712a7621005016e784ab35ead9d895d0f
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-10-19 09:59:00 -0500

    feat: adding framer motion and page transitions

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,12 @@
+import { PageWrapper } from "@/components/common/PageWrapper";
+
 const Blog = () => {
   return (
-    <div>
+    <PageWrapper className="flex flex-col lg:flex-row gap-8 relative">
       <h1 className="text-[200px] text-slate-200 font-bold text-center">
         BLOG
       </h1>
-    </div>
+    </PageWrapper>
   );
 };
 
--- components/common/PageWrapper.tsx
+++ components/common/PageWrapper.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { classNames } from "@/utils/strings";
 import { PropsWithChildren } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 type PageWrapperProps = PropsWithChildren & {
   className?: string;
@@ -7,10 +10,16 @@ type PageWrapperProps = PropsWithChildren & {
 
 export const PageWrapper = ({ children, className }: PageWrapperProps) => {
   return (
-    <article
-      className={classNames("min-h-screen pt-8", className ? className : "")}
-    >
-      {children}
-    </article>
+    <AnimatePresence>
+      <motion.article
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        transition={{ delay: 0.25 }}
+        className={classNames("min-h-screen pt-8", className ? className : "")}
+      >
+        {children}
+      </motion.article>
+    </AnimatePresence>
   );
 };
