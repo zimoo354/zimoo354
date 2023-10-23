8461be2a00f511f7bb523a026e1fdec763a52144
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-10-23 15:26:54 -0500

    typo

--- constants/content/profile.tsx
+++ constants/content/profile.tsx
@@ -8,7 +8,7 @@ export const profile = {
       <br />
       I'm a Full-stack Software Engineer with 9 years of experience specialized
       in building user-centric products that deliver real value by leveraging a
-      wide range of technologies and skill, including (but not limited to)
+      wide range of technologies and skills, including (but not limited to){" "}
       <b>
         Javascript/Typescript, React, Next.js, Solidity, SQL/No-SQL, AWS, and
         more.
