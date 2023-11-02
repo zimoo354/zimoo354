f84330936736cee4b9b216f694ae0b1e5f056dca
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-11-02 14:50:45 -0500

    feat: adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { pageTransitions } from "@/utils/animations";
 import "./styles/globals.css";
 import { NoiseOverlay } from "@/components/common/NoiseOverlay";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
@@ -21,6 +22,19 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </motion.main>
       </motion.div>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-98280332-2"
+      />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-98280332-2');
+        `}
+      </Script>
     </AnimatePresence>
   );
 };
