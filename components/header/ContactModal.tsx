c03cbcecc5a6192cd8b5500bec11e60f3571890e
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-11-02 14:32:44 -0500

    Update ContactModal.tsx

--- components/header/ContactModal.tsx
+++ components/header/ContactModal.tsx
@@ -42,6 +42,7 @@ export const ContactModal = () => {
         "fixed h-screen w-screen top-0 left-0 bg-white/30 backdrop-blur-md z-10 transition-all duration-500 overflow-hidden origin-bottom cursor-pointer",
         open ? "scale-1" : "scale-0"
       )}
+      onKeyDown={() => {}}
     >
       <motion.span
         className="fixed font-[100] text-xl text-slate-600 hidden md:block"
@@ -54,6 +55,7 @@ export const ContactModal = () => {
       </motion.span>
       <div
         onClick={(evt) => evt.stopPropagation()}
+        onKeyDown={() => {}}
         className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-fit w-[480px] max-w-[95vw] bg-white border rounded-lg py-8 px-4 cursor-default"
       >
         <button
