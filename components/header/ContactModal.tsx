cc9a0eb722967e3601ec568644fe5918ede97fc3
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-11-08 23:04:25 -0600

    fix: dark mode contact modal

--- components/header/ContactModal.tsx
+++ components/header/ContactModal.tsx
@@ -39,13 +39,13 @@ export const ContactModal = () => {
       onClick={toggle}
       onMouseMove={handleMouseMove}
       className={classNames(
-        "fixed h-screen w-screen top-0 left-0 bg-white/30 backdrop-blur-md z-10 transition-all duration-500 overflow-hidden origin-bottom cursor-pointer",
+        "fixed h-screen w-screen top-0 left-0 bg-white/30 dark:bg-black/30 backdrop-blur-md z-10 transition-all duration-500 overflow-hidden origin-bottom cursor-pointer",
         open ? "scale-1" : "scale-0"
       )}
       onKeyDown={() => {}}
     >
       <motion.span
-        className="fixed font-[100] text-xl text-slate-600 hidden md:block"
+        className="fixed font-[100] text-xl text-slate-600 dark:text-slate-400 hidden md:block"
         style={{
           x,
           y,
@@ -56,7 +56,7 @@ export const ContactModal = () => {
       <div
         onClick={(evt) => evt.stopPropagation()}
         onKeyDown={() => {}}
-        className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-fit w-[480px] max-w-[95vw] bg-white border rounded-lg py-8 px-4 cursor-default"
+        className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-fit w-[480px] max-w-[95vw] bg-white dark:bg-slate-900 border border-slate-400 rounded-lg py-8 px-4 cursor-default"
       >
         <button
           onClick={toggle}
